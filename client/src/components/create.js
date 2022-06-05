a5010c06d813ee5d9a11f8986bb115f211383cdb
Stormtroopper <75199964+Stormtroopper@users.noreply.github.com>
2022-06-05 17:11:08 +0530

    Add files via upload

---
+++ client/src/components/create.js
@@ -0,0 +1,97 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router";
+
+export default function Create() {
+ const [form, setForm] = useState({
+   name: "",
+   position: "",
+   level: "",
+ });
+ const navigate = useNavigate();
+
+ // These methods will update the state properties.
+ function updateForm(value) {
+   return setForm((prev) => {
+     return { ...prev, ...value };
+   });
+ }
+
+ // This function will handle the submission.
+ async function onSubmit(e) {
+   e.preventDefault();
+
+   // When a post request is sent to the create url, we'll add a new record to the database.
+   const newPerson = { ...form };
+
+   await fetch("http://localhost:5000/record/add", {
+     method: "POST",
+     headers: {
+       "Content-Type": "application/json",
+     },
+     body: JSON.stringify(newPerson),
+   })
+   .catch(error => {
+     window.alert(error);
+     return;
+   });
+
+   setForm({ name: "", position: "", level: "" });
+   navigate("/");
+ }
+
+ // This following section will display the form that takes the input from the user.
+ return (
+   <div>
+     <h3>Create New Artist</h3>
+     <form onSubmit={onSubmit}>
+       <div className="form-group">
+         <label htmlFor="name">Artist Name</label>
+         <input
+           type="text"
+           className="form-control"
+           id="name"
+           value={form.name}
+           onChange={(e) => updateForm({ name: e.target.value })}
+         />
+       </div>
+       <div className="form-group">
+         <label htmlFor="position">Bio</label>
+         <input
+           type="text"
+           className="form-control"
+           id="position"
+           value={form.position}
+           onChange={(e) => updateForm({ position: e.target.value })}
+         />
+       </div>
+       <div className="form-group">
+         <div className="form-check form-check-inline">
+           <input
+             className="form-check-input"
+             type="date"
+             name="positionOptions"
+             id="positionIntern"
+             value="Intern"
+             onChange={(e) => updateForm({ level: e.target.value })}
+           />
+         <label htmlFor="positionIntern" className="form-check-label">Date of Birth</label>
+           <input
+             type="text"
+             className="form-control"
+             id="position"
+             value={form.level}
+             onChange={(e) => updateForm({ position: e.target.value })}
+           />
+         </div>
+       </div>
+       <div className="form-group">
+         <input
+           type="submit"
+           value="Create person"
+           className="btn btn-primary"
+         />
+       </div>
+     </form>
+   </div>
+ );
+}
